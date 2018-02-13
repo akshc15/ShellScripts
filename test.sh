3a526830194dc83bbdeeb8c5f7cb754bc4477054
akshc15 <chetulegaurav@gmail.com>
2018-02-13 19:39:48 +0000

    added two options for mail

--- test.sh
+++ test.sh
@@ -19,9 +19,9 @@ data2=` df | grep /dev/xvda2 | awk '{print $5}' | cut -d"%" -f1`
 if [ $data1 -gt 2 ] || [ $data -lt 2 ]
 then
 #mail using mail command and service
-#echo "The usage for /test = $data1 %" | `mail -s "Usage change in /test:" user@domain.com`
+#echo "The usage for /test = $data1 %" | `mail -s "Usage change in /test:" akshay.chetule@gmail.com`
 #mail using sendmail service
-echo "The usage for /test = $data1 %" | `sendmail user@domain.com`
+echo "The usage for /test = $data1 %" | `sendmail akshay.chetule@gmail.com`
 echo "change in usage for /test at `date | awk '{print $2 " " $3 " " $4}'` = $data1" >> /root/log
 else
 echo "There is no change Usage at `date | awk '{print $2 " " $3 " " $4}'` is = $data1 %" >> /root/nochange 
@@ -31,10 +31,11 @@ fi
 if [ $data2 -gt 13 ] || [ $data2 -lt 13 ]
 then
 #mail using mail command and service
-#echo "The usage for / = $data2 %" | `mail -s "Usage change in /:" user@domain.com`
+#echo "The usage for / = $data2 %" | `mail -s "Usage change in /:" akshay.chetule@gmail.com`
 #mail using sendmail service
-echo "The usage for / = $data2 %" | `sendmail user@domain.com`
+echo "The usage for / = $data2 %" | `sendmail akshay.chetule@gmail.com`
 echo "change in usage for / at `date | awk '{print $2 " " $3 " " $4}'` = $data2" >> /root/log2
 else
+#echo "The usage for / = $data2 %" | `mail -s "Usage change in /:" akshay.chetule@gmail.com`
 echo "There is no change Usage at `date | awk '{print $2 " " $3 " " $4}'` is = $data2 %" >> /root/nochange2 
 fi

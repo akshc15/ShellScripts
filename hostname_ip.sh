1cb1fcfe95ac04c4f338f05b23a98d4c9e0c3b86
akshc15 <34731524+akshc15@users.noreply.github.com>
2021-07-26 16:07:41 +0530

    Update hostname_ip.sh

--- hostname_ip.sh
+++ hostname_ip.sh
@@ -2,6 +2,7 @@
 
 #create file name ip with all IPs in it
 #create file hostname with all respective hostnames in it
+##if servers are passwordless from host machine then will be very easy to work with this script###
 
 index_array=(`cat ip`)
 index_array2=(`cat hostname`)
